99446d8a696991c9b8b0dd7a22ab74644c75290f
shabdendhu <tshabdendhu@gmail.com>
2021-03-07 17:50:38 +0530

    clleaning up

--- config.js
+++ config.js
@@ -1,20 +1,9 @@
 module.exports = {
-  mysqldb: {
-    host: "localhost",
-    user: "workbench",
-    password: "manage",
-    database: "database_2",
-    dateStrings: true,
-  },
-  //  {
-  //   host: "localhost",
-  //   user: "root",
-  //   password: "root",
-  //   database: "careipro",
-  //   dateStrings: true
-  // },
-  // client_access_origins: ["http://localhost:3000", "http://m.careipro.com"],
-  // assets_url: "http://localhost:8080/doctorapp/assets/",
-  // visiter_log_file_path: "C:/wamp64/www/doctorapp/assets/files/logs"
-  //assets_url:'https://careipro.com/assets/'
+	mysqldb: {
+		host: "127.0.0.1",
+		user: "admin",
+		password: "manage",
+		database: "database_3",
+		dateStrings: true,
+	},
 };
